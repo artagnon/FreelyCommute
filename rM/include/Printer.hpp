302e181ad8a54ade88ae43580133140130287ea0
Ramkumar Ramachandra <artagnon@gmail.com>
2020-09-20 02:39:44 +0200

    [Printer] reinterpret and print points
    Seems to work, modulo some issues towards the end of the output.

--- rM/include/Printer.hpp
+++ rM/include/Printer.hpp
@@ -7,25 +7,25 @@ namespace fc::rM
 {
   inline std::ostream &operator<<(std::ostream &OS, const Point &Obj)
   {
-    OS << "Point has " << std::dec << +Obj.NChildren << " children" << std::endl;
+    OS << std::string(3, ' ') << "{" << reinterpret_cast<const float &>(Obj.X) << ", " << reinterpret_cast<const float &>(Obj.Y) << ", " << reinterpret_cast<const float &>(Obj.Speed) << ", " << reinterpret_cast<const float &>(Obj.Direction) << ", " << reinterpret_cast<const float &>(Obj.Width) << ", " << reinterpret_cast<const float &>(Obj.Pressure) << "}" << std::endl;
     std::for_each(Obj.Children.begin(), Obj.Children.end(), [&OS](auto Child) { OS << Child; });
     return OS;
   }
   inline std::ostream &operator<<(std::ostream &OS, const Line &Obj)
   {
-    OS << "Line has " << std::dec << +Obj.NChildren << " children" << std::endl;
+    OS << std::string(2, ' ') << std::dec << +Obj.NChildren << " points:" << std::endl;
     std::for_each(Obj.Children.begin(), Obj.Children.end(), [&OS](auto Child) { OS << Child; });
     return OS;
   }
   inline std::ostream &operator<<(std::ostream &OS, const Layer &Obj)
   {
-    OS << "Layer has " << std::dec << +Obj.NChildren << " children" << std::endl;
+    OS << std::string(1, ' ') << std::dec << +Obj.NChildren << " lines:" << std::endl;
     std::for_each(Obj.Children.begin(), Obj.Children.end(), [&OS](auto Child) { OS << Child; });
     return OS;
   }
   inline std::ostream &operator<<(std::ostream &OS, const Page &Obj)
   {
-    OS << "Page has " << std::dec << +Obj.NChildren << " children" << std::endl;
+    OS << std::dec << +Obj.NChildren << " layers:" << std::endl;
     std::for_each(Obj.Children.begin(), Obj.Children.end(), [&OS](auto Child) { OS << Child; });
     return OS;
   }
