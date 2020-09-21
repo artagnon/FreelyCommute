2c1baeb7f34f0ee42b0f6c1cf91f99b85e443f76
Ramkumar Ramachandra <artagnon@gmail.com>
2020-09-21 18:28:05 +0200

    [Parser] Remove ugly recursion
    Use parameter packs to build a recursive function guarded by `if
    constexpr`.

--- rM/include/Parser.hpp
+++ rM/include/Parser.hpp
@@ -16,7 +16,7 @@ namespace fc::rM
     operator T()
     {
       auto [M, Sz] = tablemap::M<T>;
-      for (auto i = 0; i < Sz; ++i)
+      for (size_t i = 0; i < Sz; ++i)
       {
         i64 S = 0;
         Stream.read(reinterpret_cast<char *>(&S), sizeof(S));
@@ -31,17 +31,33 @@ namespace fc::rM
     std::ifstream &Stream;
     Page Root;
 
-    template <typename ParentTy, typename ChildTy>
-    void fillChildren(ParentTy &B)
+    template <typename TyL, typename TyR>
+    void fillChildren(TyL &E)
     {
-      for (i32 i = 0; i < B.NChildren; ++i)
+      for (i32 i = 0; i < E.NChildren; ++i)
       {
-        B.Children.push_back(MiniParser<ChildTy>{Stream});
+        E.Children.push_back(MiniParser<TyR>{Stream});
       }
-    }
+    };
+
+    template <size_t i, typename... Ts, typename CurTy>
+    void recurseFillChildren(CurTy &E)
+    {
+      using PackTy = std::variant<Ts...>;
+      static_assert(std::is_same_v<CurTy, std::variant_alternative_t<i - 1, PackTy>>);
+      using TyL = std::variant_alternative_t<i - 1, PackTy>;
+      using TyR = std::variant_alternative_t<i, PackTy>;
+
+      fillChildren<TyL, TyR>(E);
+      if constexpr (i + 1 < sizeof...(Ts))
+        std::for_each(E.Children.begin(), E.Children.end(), [this](auto &E) { recurseFillChildren<i + 1, Ts...>(E); });
+    };
 
   public:
-    Parser(std::ifstream &S);
+    inline Parser(std::ifstream &S) : Stream(S), Root(std::move(MiniParser<Page>{Stream}))
+    {
+      recurseFillChildren<1, Page, Layer, Line, Point>(Root);
+    }
     inline operator Page() const { return Root; }
   };
 } // namespace fc::rM
